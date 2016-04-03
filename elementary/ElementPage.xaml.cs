ae738bbffc7d9fb49e5ffad2294e4c6399929474
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-04-02 20:30:33 -0700

    Remove unused using

--- elementary/ElementPage.xaml.cs
+++ elementary/ElementPage.xaml.cs
@@ -20,7 +20,6 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
 */
-using elementary.Model;
 using elementary.ViewModel;
 using System;
 using Windows.System;
